62b6c17dd8d2b7da71fe9a55e1860028e19f9333
pabla <pablodelfantexp@gmail.com>
2024-11-30 13:33:37 -0300

    chore: Update layout and content in +layout.svelte and +page.svelte for improved structure and branding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,17 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+  import Header from "./Header.svelte";
+  import "./styles.css";
 </script>
 
-<div class="app">
-	<Header />
+<div class="app bg-[FAF9F9]">
+  <Header />
 
-	<main>
-		<slot />
-	</main>
+  <main>
+    <slot />
+  </main>
 
-	<footer>
-	</footer>
+  <footer></footer>
 </div>
 
 <style>
-
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,60 @@
 </script>
 
 <svelte:head>
-  <title>Portfolio - Mavic</title>
-  <meta name="description" content="Landing portfolio for Mavic" />
+  <title>mavic</title>
+  <meta
+    name="description"
+    content="Hola! Soy Victoria Siempre me ha gustado comunicar y he ido encontrando
+        en la expresión gráfica y artística el medio para conectar ideas.
+        
+        
+         La comunicación es el eje. La comunicación como un todo entramado,
+        conectado. Las conexiones y relaciones entre las cosas es lo que me
+        importa. Pensar como sistema interconectado, mundo de mundos actuando en
+        conjunto, simbióticamente."
+  />
+
+  <!-- add icon for website -->
+  <link rel="icon" type="image/png" href="/website-icon.ico" sizes="16x16" />
 </svelte:head>
 
 <section class="container mx-auto px-4 py-8">
   <!-- Header -->
   <header class="text-center mb-12 grid gap-24">
     <div class="flex justify-between">
-      <h1 class="text-4xl font-bold mb-4">Mavic</h1>
-      <div>[XXX]</div>
+      <h1 class="text-4xl font-bold mb-4">mavic</h1>
+      <div>
+        <ul class="flex gap-3 items-center">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/victoria-rodriguez-morales-/"
+              rel="noopener noreferrer nofollow"
+              target="_blank"
+            >
+              <img src="/linkedin.png" alt="linkedin" />
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://www.instagram.com/mavicccc/"
+              rel="noopener noreferrer nofollow"
+              target="_blank"
+              ><img src="/instagram.png" alt="instagram" />
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="mailto:victoria.050@hotmail.com"
+              rel="noopener noreferrer nofollow"
+              target="_blank"
+            >
+              <img src="/mail.png" alt="mail" /></a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
     <div>
       <p class="text-gray-700 text-lg max-w-3xl mx-auto mb-5">
@@ -30,8 +74,8 @@
 </section>
 
 <section class="grid md:grid-cols-[2.121fr_4fr] gap-3 container m-auto">
-  <img src="/tazon.jpg" alt="" class="w-full" />
-  <img src="/paper.jpg" alt="" class="w-full" />
+  <img src="/tazon.jpg" alt="tazon" class="w-full" />
+  <img src="/paper.jpg" alt="paper" class="w-full" />
 </section>
 
 <style>
